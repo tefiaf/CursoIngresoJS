6f721e74349f728485e6277df89520532ed4feb3
tefiaf <43156572+tefiaf@users.noreply.github.com>
2019-07-04 12:00:50 -0300

    ej2if

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,7 +1,12 @@
 function mostrar()
 {
 //tomo la edad  
-
+    var edad;
+    edad=document.getElementById('edad').value;
+    if(edad >17)
+    {
+        alert(edad);
+    }
 
 
 }//FIN DE LA FUNCIÓN
