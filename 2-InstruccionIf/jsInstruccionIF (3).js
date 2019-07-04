6314f4ee226e11251ab813440d6307a192e8d535
tefiaf <43156572+tefiaf@users.noreply.github.com>
2019-07-04 12:03:58 -0300

    ej3if

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,17 @@
 function mostrar()
 {
 //tomo la edad  
+    var edad;
 
+    edad=document.getElementById('edad').value;
 
+    if(edad >17)
+    {
+        alert("usted es mayor de edad");
+    }
+
+    if(edad <18)
+    {
+        alert("usted es menor de edad");
+    }
 }//FIN DE LA FUNCIÓN
