926537bbee1ead15aeb61b4e46ff02958b602289
tefiaf <43156572+tefiaf@users.noreply.github.com>
2019-07-04 10:56:34 -0300

    ej9

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,17 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+    var sueldo;
+    var resultado;
+    
+    sueldo=document.getElementById('sueldo').value;
+    
+    sueldo=parseInt(sueldo);
+    resultado=parseInt(resultado);
+    
+    resultado=sueldo*10/100;
 
+    document.getElementById('resultado').value=resultado;
+    
 	
 }
