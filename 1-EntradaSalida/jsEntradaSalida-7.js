5bd0e6e57aa0819437cbe04f8184738e1df075ae
tefiaf <43156572+tefiaf@users.noreply.github.com>
2019-07-04 10:26:54 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,61 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+    var numeroUno;
+    var numeroDos;
+    var sumar;
+    numeroUno=document.getElementById('numeroUno').value;
+    numeroDos=document.getElementById('numeroDos').value;
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos);
+
+    sumar=numeroUno+numeroDos;
+
+    alert("la suma es: "+sumar);
 }
 
 function restar()
 {
-	
+    var numeroUno;
+    var numeroDos;
+    var restar;
+    numeroUno=document.getElementById('numeroUno').value;
+    numeroDos=document.getElementById('numeroDos').value;
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos);
+
+    restar=numeroUno-numeroDos;
+
+    alert("la resta es: "+restar);
 }
 
 function multiplicar()
 { 
-	
+    var numeroUno;
+    var numeroDos;
+    var multiplicar;
+    numeroUno=document.getElementById('numeroUno').value;
+    numeroDos=document.getElementById('numeroDos').value;
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos);
+
+    multiplicar=numeroUno*numeroDos;
+
+    alert("la multiplicación es: "+multiplicar)
 }
 
 function dividir()
 {
-	
+    var numeroUno;
+    var numeroDos;
+    var dividir;
+    numeroUno=document.getElementById('numeroUno').value;
+    numeroDos=document.getElementById('numeroDos').value;
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos);
+
+    dividir=numeroUno/numeroDos;
+
+    alert("la división es: "+dividir);
 }
 
