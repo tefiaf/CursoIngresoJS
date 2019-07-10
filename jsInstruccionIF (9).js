1a4c3449eb6a76c57f092ce07b78e4308aeffacb
tefiaf <43156572+tefiaf@users.noreply.github.com>
2019-07-10 10:07:10 -0300

    ej9if

---
+++ jsInstruccionIF (9).js
@@ -0,0 +1,10 @@
+function mostrar()
+{
+	
+
+	//Genero el número RANDOM entre 1 y 10 
+ 	var numero= Math.floor(Math.random()*10)+1;
+	//"math" va con mayuscula sino, no es lo mismo. math se utiliza para obtener un numero random al azar
+	// despues de la multiplicacion va entre parentesis (max-min)+min
+	alert(numero);
+}//FIN DE LA FUNCIÓN
