8445a0cfd0f7f26758a0b3b87feb8b39db4913dc
tefiaf <43156572+tefiaf@users.noreply.github.com>
2019-07-10 11:14:33 -0300

    ej1switch

---
+++ jsInstruccionSwitch(1).js
@@ -0,0 +1,25 @@
+function mostrar()
+{
+//tomo la edad  
+    var mes = document.getElementById('mes').value;
+    switch(mes)
+    {
+        case "Enero": alert("que comiences bien el año!!!");
+        break;
+        
+        case "Marzo":alert("a clases!!!");
+        break;
+
+        case "Julio":alert("se vienen las vacaciones!!!");
+        break;
+
+        case "Diciembre":alert("Felices fiesta!!!");
+        break;
+        //switch va con parentesis y abro llaves, y case va con comillasy termina en break.
+        //case despues de la palabra en comillas van dos puntos
+    }
+
+
+
+
+}//FIN DE LA FUNCIÓN
