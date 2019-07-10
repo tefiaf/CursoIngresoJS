3e1b3078df06595d6f5746c59a2949dc91e4a158
tefiaf <43156572+tefiaf@users.noreply.github.com>
2019-07-10 11:33:14 -0300

    ej2switch

---
+++ jsInstruccionSwitch(2).js
@@ -0,0 +1,30 @@
+function mostrar()
+{
+//tomo la edad  
+var mes = document.getElementById('mes').value;
+
+switch(mes)
+{
+    case "Enero":
+    case "Febrero":
+    case "Marzo":
+    case "Abril":
+    case "Mayo":
+    case "Junio" :alert("Falta para el invierno");
+    break;
+
+    case "Julio":
+    case "Agosto":alert("Abrigate que hace frio");
+    break;
+    
+    case "Septiembre":
+    case "Octubre":
+    case "Noviembre":
+    case "Diciembre":alert("Ya pasamos el frio, ahora calor!!!");
+    break;
+
+}
+
+
+
+}//FIN DE LA FUNCIÓN
