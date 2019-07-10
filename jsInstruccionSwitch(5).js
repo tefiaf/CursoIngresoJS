aa378a094dbadfb21aa4a1765763b034f9013111
tefiaf <43156572+tefiaf@users.noreply.github.com>
2019-07-10 11:51:02 -0300

    ej5switch

---
+++ jsInstruccionSwitch(5).js
@@ -0,0 +1,19 @@
+function mostrar()
+{
+//tomo la edad  
+var hora = document.getElementById('hora').value;
+alert (hora);
+    switch(hora)
+    {
+        case "7":
+        case "8":
+        case "9":
+        case "10":
+        case "11": alert ("Es de mañana");
+        break;
+    }
+	
+
+
+
+}//FIN DE LA FUNCIÓN
