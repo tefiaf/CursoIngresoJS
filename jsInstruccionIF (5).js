f1d1c73e9f586863458eea88f60d792804f8796d
tefiaf <43156572+tefiaf@users.noreply.github.com>
2019-07-10 09:19:11 -0300

    ej5if

---
+++ jsInstruccionIF (5).js
@@ -0,0 +1,15 @@
+function mostrar()
+{
+//tomo la edad  
+    var edad;
+    edad=document.getElementById('edad').value;
+    if (edad <12)
+
+    {
+        alert("usted no es adolescente");
+    }
+    if (edad >18)
+    {
+        alert("usted no es adolescente");
+    }
+}//FIN DE LA FUNCIÓN
