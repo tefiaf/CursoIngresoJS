b5a304d47088d7e44b433b32d8992ade0b9496bb
stephanie <tefiaf@example.com>
2019-07-11 12:21:01 -0300

    recuperatorio ej4 incompleto

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,24 @@
 function mostrar()
 {
+    var numeroUno;
+    var numeroDos;
+    var resultado;
+    
+    
+    numeroUno=prompt("Ingrese el primer numero por favor");
+    numeroDos=prompt("Ingrese el segundo numero por favor");
+   
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos);
+   
+    if (numeroUno==numeroDos)
+    {
+        resultado=(numeroUno+numeroDos);
+    }else
+    {
+        numeroUno=parseInt(numeroUno);
+        numeroDos=parseInt(numeroDos);
 
+       
+    }
 }
