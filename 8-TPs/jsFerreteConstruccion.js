68e2d0b2709486de61a68d2efbed77b5ffdc8782
stephanie <tefiaf@example.com>
2019-07-11 10:26:54 -0300

    ferrete construccion parte1

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,6 +6,17 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+    var largo;
+    var ancho;
+    var perimetro;
+    var alambre;
+    largo=document.getElementById('Largo').value;
+    ancho=document.getElementById('Ancho').value;
+    largo=parseInt(largo);
+    ancho=parseInt(ancho);
+    perimetro=(largo+ancho)*2;
+    alambre=perimetro/3;
+    alert("cada alambre debe medir "+alambre+" cm.")
 
 }
 function Circulo () 
