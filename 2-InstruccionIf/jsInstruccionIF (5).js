493005dde1084def254eb21788e530f87d852d9b
stephanie <tefiaf@example.com>
2019-07-11 10:54:51 -0300

    ej5if modificado en clase

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,21 @@
 function mostrar()
 {
 //tomo la edad  
-
-
+    var precio;
+    precio=document.getElementById('edad').value;
+    if (precio<25)
+    {
+        alert("Barato");
+    }else
+    {
+        if (precio>75)
+        {
+            alert("caro");
+        }else
+        {
+            alert("bien");
+        }
+    
+    }
+    
 }//FIN DE LA FUNCIÓN
