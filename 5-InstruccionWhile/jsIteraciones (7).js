e30ca03a5438e5ecea5fafc0ea31d7d6888f3a0a
stephanie <stefifernandez97@gmail.com>
2019-07-17 12:21:29 -0300

    ejercicio 7 incompleto

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,7 +4,22 @@ function mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
-
+	var numero;
+	var promedio;
+	var suma;
+	
+	while(respuesta!="si"){
+		contador++;
+		numero=parseInt(numero);
+		numero=prompt("ingrese un numero por favor");
+		while(isNaN(numero)){
+			numero=parseInt(numero);
+			numero=prompt("dato incorrecto");
+		}
+		acumulador+=numero;
+	
+	}	numero=prompt("desea seguir ingresando numeros?")
+	promedio=acumulador/contador;
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
