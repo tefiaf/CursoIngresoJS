50866aabc094203c79bb1bcddbd3b95e3d105301
stephanie <stefifernandez97@gmail.com>
2019-07-17 11:55:59 -0300

    ej6 incompleto while

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,10 +3,20 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var numero;
+
+	numero=parseInt(numero);
+	numero=prompt("ingrese cinco numeros por favor");
+
+	while(isNaN(numero)){
+		numero=prompt("dato incorrecto, usted ingreso una letra");
+	}
+	while(contador<5){
+		numero=acumulador++;
+	}
 	
 
-
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+	document.getElementById('suma').value=acumulador;
+	document.getElementById('promedio').value=acumulador/5;
 
 }//FIN DE LA FUNCIÓN
