b5f3061ae36799faabdb1f31e05cad4bbf7ac273
stephanie <stefifernandez97@gmail.com>
2019-07-17 12:12:56 -0300

    ej6 while corregido

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -4,17 +4,27 @@ function mostrar()
 	var contador=0;
 	var acumulador=0;
 	var numero;
+	var promedio;
+	var suma;
 
-	numero=parseInt(numero);
-	numero=prompt("ingrese cinco numeros por favor");
+	
 
-	while(isNaN(numero)){
-		numero=prompt("dato incorrecto, usted ingreso una letra");
-	}
+	
 	while(contador<5){
-		numero=acumulador++;
+		contador++;
+		
+		numero=prompt("ingrese cinco numeros por favor");
+		numero=parseInt(numero);
+		while(isNaN(numero)){
+			numero=prompt("dato incorrecto, usted ingreso una letra");
+			numero=parseInt(numero);//lo parseo ya que si no es un numero de todas formas va a seguir con ese dato 
+		
+		}
+		acumulador+=numero;//el acumulador esta adentro del while ya que tiene que ir guardando todos los numeros
 	}
 	
+	promedio=acumulador/contador;
+
 
 	document.getElementById('suma').value=acumulador;
 	document.getElementById('promedio').value=acumulador/5;
