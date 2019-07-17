3e56e8c263bdfd1a1397677d7ea5e0b7a8347504
stephanie <stefifernandez97@gmail.com>
2019-07-17 10:54:55 -0300

    ej3 y4 while

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,10 @@
 function mostrar()
 {
-
-var clave = prompt("ingrese el número clave.");
+    var clave;
+    while (clave!="utn750"){
+        clave=prompt("Ingrese la clave por favor");
+    }
+//var clave = prompt("ingrese el número clave.");
 
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,13 @@
 function mostrar()
 {
-
-	var numero = prompt("ingrese un número entre 0 y 10.");
+	var numero = prompt ("Ingrese un número del uno al diez por favor" );
+	
+	
+	while(numero >10){
+		numero=parseInt(numero);
+		numero=prompt("Incorrecto, ingrese nuevamente");
+	}
+	//var numero = prompt("ingrese un número entre 0 y 10.");
 
 
 }//FIN DE LA FUNCIÓN
