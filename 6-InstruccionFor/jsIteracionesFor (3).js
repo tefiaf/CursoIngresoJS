0c7175128ae36934193888731aabbc032d72a3a8
tefiaf <43156572+tefiaf@users.noreply.github.com>
2019-07-31 11:17:55 -0300

    123

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,16 @@
 function mostrar()
 {
-
-var repetciones = prompt("ingrese el número de repeticiones");
-
+    var cant;
+    cant=prompt("cuantas veces quiere repetir");
+    //var repeticiones = prompt("ingrese el número de repeticiones");
+    for(var i=cant;i>0;cant--)
+    {
+        //console.log("Hola UTN FRA");
+        if(cant==1000){ 
+            console.log("Hola UTN FRA");
+            break;
+        }
+    }
 
 
 }//FIN DE LA FUNCIÓN
