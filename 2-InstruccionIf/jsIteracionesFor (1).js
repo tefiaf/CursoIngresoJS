b7f5ff6d65b02495f20d924fb4494c9c43aab92c
tefiaf <43156572+tefiaf@users.noreply.github.com>
2019-07-31 11:16:41 -0300

    for 1 2 3

---
+++ 2-InstruccionIf/jsIteracionesFor (1).js
@@ -0,0 +1,11 @@
+function mostrar()
+{
+    //las variables fuera del for no existe
+    //primera parte declara la variable, lo segundo pone la condicion, ejecuta la condicion, ejecuta el codigo
+    //y modifica
+    // puedo no poner nada adentro de los parentesis pero si o si tienen que estar los punto y  coma
+    var cont =0;
+    for (cont=1;cont<11;cont++){
+        document.write(cont);
+    }
+}
---
+++ 2-InstruccionIf/jsIteracionesFor (2).js
@@ -0,0 +1,9 @@
+function mostrar()
+{
+    var cont=0;
+    for (cont=10;cont>0;cont--)
+    {
+        document.write(cont);
+    }
+
+}
